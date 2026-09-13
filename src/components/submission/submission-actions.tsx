ee40c3d4e926246c9422ca7208486c190e74eb05
Jedrick Ocenar <jdpocenar@mymail.mapua.edu.ph>
2026-09-13 14:01:20 +0800

    submissions changes

--- src/components/submission/submission-actions.tsx
+++ src/components/submission/submission-actions.tsx
@@ -17,19 +17,7 @@ export function SubmissionActions({
   onSubmit: (e: MouseEvent) => void
 }) {
   return (
-    <div className="flex flex-col items-start justify-between gap-6 border-t border-neutral-200 pt-6 sm:flex-row sm:items-center">
-      <div className="space-y-2">
-        {showNextPage ? (
-          <Button
-            type="button"
-            onClick={onNextPage}
-            className="h-10 cursor-pointer rounded-lg bg-[#242424] px-5 py-2.5 text-sm font-medium text-white shadow-xs hover:bg-black"
-          >
-            Next page
-          </Button>
-        ) : null}
-      </div>
-
+    <div className="flex flex-col items-start justify-end gap-6 border-t border-neutral-200 pt-6 sm:flex-row sm:items-center">
       <div className="flex w-full items-center justify-end gap-3 sm:w-auto">
         <button
           type="button"
@@ -39,14 +27,24 @@ export function SubmissionActions({
           <DownloadIcon className="h-3.5 w-3.5" />
           Save as PDF
         </button>
-        <Button
-          type="button"
-          onClick={onSubmit}
-          disabled={isSubmitting}
-          className="h-10 min-w-36 cursor-pointer rounded-lg bg-[#0B6623] px-10 py-2.5 text-sm font-semibold text-white shadow-sm transition-all hover:bg-[#084D1A]"
-        >
-          {isSubmitting ? "Submitting..." : "Submit"}
-        </Button>
+        {showNextPage ? (
+          <Button
+            type="button"
+            onClick={onNextPage}
+            className="h-10 min-w-36 cursor-pointer rounded-lg bg-[#242424] px-10 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-black"
+          >
+            Next page
+          </Button>
+        ) : (
+          <Button
+            type="button"
+            onClick={onSubmit}
+            disabled={isSubmitting}
+            className="h-10 min-w-36 cursor-pointer rounded-lg bg-[#0B6623] px-10 py-2.5 text-sm font-semibold text-white shadow-sm transition-all hover:bg-[#084D1A]"
+          >
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </Button>
+        )}
       </div>
     </div>
   )
